d57e98a7e75b5585c81dab59cf0ea88e0e066afa
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2024-03-19 23:33:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 15178129
 API_HASH     = "274a1c7f7ea99a473c6bd8facebc59ed"
 BOT_TOKEN    = "5323421782:AAFMaCslkGl5adfv7SkZ5_2jICXtkgXLL1k"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001652082524
 ADMIN        = 1905890740
 CHANNEL      = "@GreyMatter_Bots"
