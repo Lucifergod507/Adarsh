c488ec3dc693e9bff32a7299a6ed4603a8a3efa9
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2024-03-19 20:53:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ""]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS = "1112773045"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5323421782:AAFMaCslkGl5adfv7SkZ5_2jICXtkgXLL1k")
+    API_ID = int(os.environ["API_ID", "274a1c7f7ea99a473c6bd8facebc59ed"]
+    API_HASH = os.environ["API_HASH", "15178129"]
+    AUTH_USERS = "1905890740"
 
