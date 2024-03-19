9f7506ca4be2b9b8b4d900c3de340debe6865251
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2024-03-19 21:06:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,7 @@
 
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    pass
-    """
+
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5323421782:AAFMaCslkGl5adfv7SkZ5_2jICXtkgXLL1k")
     API_ID = int(os.environ["API_ID", "274a1c7f7ea99a473c6bd8facebc59ed"]
     API_HASH = os.environ["API_HASH", "15178129"]
